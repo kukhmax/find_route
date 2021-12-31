7fcad8e3a64b01a83fadadd17080cae170f958c1
kukhmax <kukhmax@gmail.com>
2021-12-31 22:57:15 +0100

    add app City

--- travel/urls.py
+++ travel/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from travel import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cities/', include(('cities.urls', 'cities'))),  # 'cities:home or cities:detail
+    path('', views.home, name='home'),
+    path('about/', views.about)
 ]
